c2d2a5fcf5c90ba481b4af73084df284ab72d762
Clif <klixky@gmail.com>
2025-07-29 13:01:43 -0400

    clif-moneywash

---
+++ fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'bodacious'
+author 'Clif Development'
+description 'Clif Money Wash'
+game 'gta5'
+lua54 'yes'
+
+shared_scripts {
+    'config/config.lua',
+    '@ox_lib/init.lua',
+    'code/shared.lua'
+}
+
+client_scripts {
+    'code/client.lua'
+}
+
+server_scripts {
+    'code/server.lua'
+}
+
+files {
+    'html/*.js',
+    'html/*.html',
+    'html/*.css',
+    'html/images/*.png',
+    'html/sounds/*.ogg'
+}
+
+ui_page 'html/index.html'
+
+dependencies {
+    'ox_lib'  
+}
